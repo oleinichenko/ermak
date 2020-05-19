1fabe0643e7a34dac3920d98823b9576883d94cf
ilya oleinichenko <ilich1331@gmail.com>
2020-05-19 18:02:58 +0300

    обновил меню, добавил футер и отзывы

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 $(document).ready(function() {
-	$("#phone").mask("+7(999)999-99-99");
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
+	$("#phone").mask("+7 (999) 99-99-999");
+	$("form").submit(function() { 
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "mail.php", 
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+			$(".popup-fade").fadeIn(500);
+			$('.popup-close').click(function() {
+				$(this).parents('.popup-fade').fadeOut();
+				return false;
+			});        
+		
+			$(document).keydown(function(e) {
+				if (e.keyCode === 27) {
+					e.stopPropagation();
+					$('.popup-fade').fadeOut();
+				}
+			});
+		
+			$('.popup-fade').click(function(e) {
+				if ($(e.target).closest('.popup').length == 0) {
+					$(this).fadeOut();					
+				}
+			});	
 			setTimeout(function() {
-				// Done Functions
 				th.trigger("reset");
 			}, 500);
 		});
 		return false;
 	});
-
+	$('a[href^="#"]').click(function(){ 
+		let anchor = $(this).attr('href');  
+		$('html, body').animate({           
+		scrollTop:  $(anchor).offset().top  
+		}, 600);                            
+	});
 });
